b3d5c6f7850d188cbcf6a36a384a6de06691364f
begoIT24 <blopezprof@gmail.com>
2024-04-13 08:38:09 +0200

    correcting delete route

--- routes/web.php
+++ routes/web.php
@@ -26,4 +26,4 @@ Route::resource('/teams', TeamController::class);  */
     Route::get('teams/{id}', [TeamController::class, 'show'])->name('teams.show');
     Route::get('teams/{id}/edit', [TeamController::class, 'edit'])->name('teams.edit');
     Route::put('teams/{id}', [TeamController::class, 'update'])->name('teams.update');
-    Route::delete('teams/{id}', [TeamController::class, 'destroy'])->name('teams.delete');
+    Route::delete('teams/{id}/delete', [TeamController::class, 'destroy'])->name('teams.delete');
